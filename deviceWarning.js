7a3ca6296f8d3e16fc00a3cf95f42d14c8da49d4
nerdyperson16 <67539794+nerdyperson16@users.noreply.github.com>
2022-10-02 13:50:08 -0700

    Updated Changes for 10-2

---
+++ deviceWarning.js
@@ -0,0 +1,6 @@
+var ua = window.navigator.userAgent;
+var isIE = /MSIE|Trident/.test(ua);
+    
+if ( isIE ) {
+    document.write("You're currently using Internet Explorer which is an outdated browser currently. Some features may not be usable and will not be considered for repairs. Thank you and sorry for the inconvenience. Please check out Google Chrome, Mozilla Firefox, Microsoft Edge, etc. for newer browsers.")
+}
